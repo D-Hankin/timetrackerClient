809ab492195cb0b7df72fa1ad64f1ff84f19f946
David Hankin <d_hankin@outlook.com>
2024-04-29 22:09:29 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+interface User {
+  username: string,
+  name: string
+}
+
+function App() {
+
+  const [user, setUser] = useState<User | null>(null)
+
+  useEffect(() => {
+    fetchUser()
+  }, [])
+
+  const fetchUser = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/api/user")
+      if (!response.ok) {
+        throw new Error('Failed to fetch user data')
+      }
+      const userData: User = await response.json()
+      setUser(userData)
+    } catch (error) {
+      console.error('Error fetching user data:', error)
+    }
+  }
+  
+
+  return (
+    <>
+      <h1>The Pit of Despair Time Management System</h1>
+      { user !== null ? <h2>{ user.username + " " + user.name }</h2> : <h2>No user found</h2>}
+    </>
+  )
+}
+
+export default App
